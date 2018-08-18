136c9baa8fce6af86c906bb48ca7eca6ed2c68fd
hnguyen16 <bobby_n475@yahoo.com>
2018-08-18 08:46:53 -0500

    Project Init Commit

---
+++ TrainingProject/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrainingProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //  This is how you print to console. 
+            Say("BT is awesome!");
+
+            Environment.Exit(-1);
+        }
+
+        public void Say(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+
+        public void PracticeLoops()
+        {
+            List<int> intList1 = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            List<int> intList2 = new List<int>();
+
+            // TODO:  Write a loop to add 1 to each value, then print each element to console.  
+
+
+            // TODO:  Write a loop to print values backward.
+
+
+            //  TODO: Populate intList2 values n+5 from intList1. 
+
+        }
+
+
+        public void PracticeOOP()
+        {
+            //  Instantiate new people object. 
+            //  name = Thi, age = 1000, weight = 200, spouse = ChunChun, numList = list with value 1,2,3,4,5,6,7,8,9,10
+            //  name = ChunChun, age = 2000, weight = 20, spouse = Thi, numList = list with value 10,9,8,7,6,5,4,3,2,1
+            //  Create a list and add these two People to the list. 
+
+
+
+            //  TODO: Set Thi's age to 10, print out his age. 
+
+            //  TODO:  
+        }
+
+    }
+}
